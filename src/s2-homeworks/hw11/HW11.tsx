963229b57a0f982a94d7a10180295b5a0ef808c7
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2024-05-28 07:26:38 +0300

    DZ10 ready

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState} from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
 import { restoreState } from '../hw06/localStorage/localStorage'
@@ -14,9 +14,12 @@ function HW11() {
     // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 50))
-    const change = (event:Event, value: any ) => {
-     setValue1(value)
-        console.log(event.currentTarget)
+    const change = (event: Event, value: any) => {
+    if(Array.isArray(value)){
+        setValue1(value[0])
+        setValue2(value[1])
+    }else
+        setValue1(value)
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
     }
 
@@ -40,7 +43,7 @@ function HW11() {
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-double-slider'}
-                            value={value2}
+                            value={[value1,value2]}
                             onChange={change}
 
                             // сделать так чтоб value1/2 изменялось // пишет студент
